a9580552ddc3531a4dcc12898702ae5dd27bf7ac
Rohan Maharjan <nahor.uzamaki@gmail.com>
2025-06-07 23:00:35 +0545

    feat: Add Day 7 Python intro, input(), len(), and string operations

---
+++ Day_07/Python_Basics/basics.py
@@ -0,0 +1,29 @@
+2 + 2        # 4
+50 - 5*6     # 20
+(50 - 5*6) / 4  # 5.0
+8 / 5        # 1.6
+17 // 3      # 5
+17 % 3       # 2
+2 ** 3       # 8
+
+
+msg = 'Hello World'
+print(msg)     # Repetition
+
+print("Hello" + " " + "Day07")
+
+print("Hello " + input("What is your name?") + "!")
+
+name = input("What is your name?")
+print(name)
+
+name = "Frog"
+length = len(name)
+print(length)
+
+print("Welcome to the Band Name Generator.")
+
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+
+print("Your band name could be: " + city + " " + pet)
