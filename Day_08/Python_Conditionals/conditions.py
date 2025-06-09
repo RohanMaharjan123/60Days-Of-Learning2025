9b92898068b1ecc78dc2a3cd5021aaea0aac5991
Rohan Maharjan <nahor.uzamaki@gmail.com>
2025-06-09 10:19:27 +0545

    feat: Add Day 8 - Python conditional statements and examples

---
+++ Day_08/Python_Conditionals/conditions.py
@@ -0,0 +1,18 @@
+age = 25
+has_license = True
+
+if age >= 18 and has_license:
+    print("You can drive.") 
+
+# or operator
+is_weekend = True
+is_holiday = False
+
+if is_weekend or is_holiday:
+    print("You can rest.")
+
+# not operator
+logged_in = False
+
+if not logged_in:
+    print("Please log in.") 
